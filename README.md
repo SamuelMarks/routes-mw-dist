dff7e5a50842d65b201eb51e5a0e1eacda011282
Samuel Marks <samuelmarks@gmail.com>
2017-08-21 01:08:38 +1000

    Init

---
+++ README.md
@@ -0,0 +1,56 @@
+routes-merger
+=============
+
+Generic--connect, restify, express--function to merge routes in from a Map.
+
+Supported 'servers':
+
+ - [restify](https://github.com/restify/node-restify)
+ 
+WiP:
+
+ - [connect](https://github.com/senchalabs/connect)
+ - [Express](https://github.com/expressjs/express)
+ - [http](https://nodejs.org/api/http.html) (Node.JS)
+ - [https](https://nodejs.org/api/https.html) (Node.JS)
+ 
+## Usage
+
+    import { routesMerger } from 'routes-merger';
+
+    routesMerger(/*IRoutesMergerConfig*/);
+
+Some other file:
+
+    export const getUser = (app: restify.Server, namespace: string = ''): void =>
+        app.get(namespace, (req: restify.Request, res: restify.Response, next: restify.Next) => {
+             req.send(200, 'I am user');
+             return next();
+        };
+
+## Configuration
+
+See `IroutesMwConfig` interface in [routes-merger.d.ts](https://github.com/SamuelMarks/routes-merger).
+
+## Extending
+
+Adding a new server? - Expand the `IroutesMwConfig` interface, and add a new short-function that implements it. See others for reference.
+
+### Development setup
+Install the latest Node.JS, `npm i -g typings typescript`, then:
+
+    git clone https://github.com/SamuelMarks/routes-merger
+    git clone https://github.com/SamuelMarks/routes-merger-dist
+    cd routes-merger
+    typings i
+    npm i
+    npm test
+
+Update [routes-merger-dist](https://github.com/SamuelMarks/routes-merger-dist):
+
+    find -type f -not -name "*.ts" -and -not -path "./.git/*" -and -not -path "./node-modules/*" -and -not -name '*.map' | cpio -pdamv ../routes-merger-dist
+
+## Future work
+
+  - Finish implementing Express, Connect and generic
+  - Tests
---
+++ index.js
@@ -0,0 +1,71 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Logger = require("bunyan");
+const restify = require("restify");
+const path_1 = require("path");
+const restifyInitApp = (app, with_app, skip_app_logging, skip_app_version_routes, package_) => {
+    if (with_app != null)
+        app = with_app(app);
+    app.use(restify.plugins.queryParser());
+    app.use(restify.plugins.bodyParser());
+    if (!skip_app_logging) {
+        const event = 'after';
+        app.on(event, restify.plugins.auditLogger({
+            event, log: Logger.createLogger({
+                name: 'audit',
+                stream: process.stdout
+            })
+        }));
+    }
+    if (!skip_app_version_routes)
+        ['/', '/version', '/api', '/api/version'].map(route_path => app.get(route_path, (req, res, next) => {
+            res.json({ version: package_.version });
+            return next();
+        }));
+    return app;
+};
+const restifyStartApp = (skip_start_app, app, listen_port, onServerStart, logger, callback) => skip_start_app ? callback != null && callback(null, app)
+    : app.listen(listen_port, () => {
+        logger.info('%s listening at %s', app.name, app.url);
+        if (onServerStart != null)
+            return onServerStart(app.url, app, callback == null ? () => { } : callback);
+        else if (callback != null)
+            return callback(void 0, app);
+        return app;
+    });
+const handleErr = (callback) => (e) => {
+    if (callback == null)
+        throw e;
+    return callback(e);
+};
+exports.routesMerger = (options) => {
+    ['routes', 'server_type', 'package_', 'app_name'].forEach(opt => options[opt] == null && handleErr(options.callback)(TypeError(`\`options.${opt}\` required.`)));
+    if (options.skip_start_app == null)
+        options.skip_start_app = false;
+    if (options.skip_app_version_routes == null)
+        options.skip_app_version_routes = false;
+    if (options.skip_app_logging == null)
+        options.skip_app_logging = false;
+    if (options.logger == null)
+        options.logger = Logger.createLogger({ name: 'routes-merger' });
+    if (options.app != null) {
+    }
+    else if (options.server_type === 'restify') {
+        options.app = restifyInitApp(options.app == null ? restify.createServer(Object.assign({ name: options.app_name }, options.createServerArgs || {}))
+            : options.app, options.with_app, options.skip_app_logging, options.skip_app_version_routes, options.package_);
+    }
+    else
+        throw Error(`NotImplemented: ${options.server_type}; TODO`);
+    const routes = new Set();
+    for (const [dir, program] of options.routes)
+        (['routes', 'route', 'admin'].some(r => dir.indexOf(r) > -1) && Object
+            .keys(program)
+            .forEach((route) => typeof program[route] === 'function' && program[route](options.app, `${options.root}/${path_1.dirname(dir)}`))
+            && routes.add(dir));
+    options.logger.info(`${options.server_type} registered routes:\t`, Array.from(routes), ';');
+    if (options.server_type === 'restify')
+        return restifyStartApp(options.skip_start_app, options.app, options.listen_port, options.onServerStart, options.logger, options.callback);
+    if (typeof options.callback === 'undefined')
+        return options.app;
+    return options.callback(void 0, options.app);
+};
